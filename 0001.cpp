0c08ffbcf152074165260b3502d80c013b37a0eb
Safin Bulat <safbulat@mail.ru>
2018-04-09 01:26:16 +0300

    first commit

---
+++ 0001.cpp
@@ -0,0 +1,25 @@
+#include <bits./stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+	
+	int a, b;
+	
+	freopen("INPUT.txt", "r", stdin);
+	cin >> a >> b;
+	fclose(stdin);
+	
+
+	
+	freopen("OUTPUT.txt", "w", stdout);
+	cout << a+b;
+	fclose(stdout);
+	
+	// новые изменения в файле
+	// новые изменения в файле 1
+	
+	
+}
